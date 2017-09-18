2dccd5a3875585d31aecf890a7e5db97737ad073
Courtney MacKenzie <courtneymackenzie@me.com>
2017-09-18 00:13:16 -0400

    First commit
    Project 1, Part 1

---
+++ Project 1/Assets/Scripts/gameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour {
+
+	float time; 
+	int myScore;
+	bool printedMessage;
+
+	// Use this for initialization
+	void Start () {
+		print ("The game has begun!");
+
+		printedMessage = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.time > 3 && printedMessage == false) {
+			// stuff that happens if true
+			print ("It's been three seconds!");
+			printedMessage = true;
+		}
+	}
+}
